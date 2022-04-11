199f385b3d088fb3e25106991fcee5c7bea4c5e0
NandhiniNadimuthu <31232923+NandhiniNadimuthu@users.noreply.github.com>
2022-04-11 14:51:41 +0530

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,43 +1,62 @@
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Student {
 
     private int id;
     private String name;
     private List<Integer> marks;
-    private Map resultMap = new HashMap();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Integer> getMarks() {
+        return marks;
+    }
+
+    public void setMarks(List<Integer> marks) {
+        this.marks = marks;
+    }
 
     public Student(int id, String name, List<Integer> marks) {
         this.id = id;
-
         this.name = name;
         this.marks = marks;
     }
 
-    public void getResult(List<Integer> mark, int id){
+    public int getResult(List<Integer> mark, int id){
         int addition=0;
         for (Integer i: mark) {
             addition = addition+i;
         }
         int average = addition/mark.size();
-        resultMap.put(id,average);
-
+        return average;
     }
     public static void main(String args[]) {
         List<Integer> list = Arrays.asList(100,80,90,70,95);
         List<Integer> list1 = Arrays.asList(70,80,70,70,50);
         Student student1 = new Student(1,"Student1", list);
-        Student student2 = new Student(1,"Student2", list1);
+        Student student2 = new Student(2,"Student2", list1);
         List<Student> students = new ArrayList<Student>();
         students.add(student1);
         students.add(student2);
-        for (Student student : students) {
-            student.getResult(student.marks,student.id);
-        }
-        for (Student student : students) {
-            System.out.println(student.resultMap);
-        }
+        Map<Integer, Integer> result = students.stream().collect(Collectors.toMap(Student::getId, Student -> Student.getResult(Student.marks, Student.id)));
+        System.out.println(result);
     }
 
 }
